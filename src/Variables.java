2e6b86600389fe20a7ee68eb0958db70efcaad79
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-05 12:54:48 -0400

    haha

--- src/Variables.java
+++ src/Variables.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Variables {
+public class Variables {
+
+    public static void main(String[] args) {
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+        myFirstNumber = 6;
+        System.out.println(myFirstNumber);
+        myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+        int myfirstnumber = 5;
+    }
 }
